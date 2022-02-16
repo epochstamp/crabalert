77c5ce8266341a94e50294fd2f48c4e089cf2731
Aittahar Samy <saittahar@uliege.be>
2022-02-16 22:55:13 +0100

    kill when too many timeouts

--- crabalert_twitter.py
+++ crabalert_twitter.py
@@ -218,7 +218,7 @@ class CrabalertTwitter:
     """
     SUBSCRIPTION MANAGEMENT
     """
-    async def _crabada_listing_alert_loop(self, seconds=5):
+    async def _crabada_listing_alert_loop(self, seconds=10):
         while True:
             try:
                 connection = open_database()
@@ -246,7 +246,7 @@ class CrabalertTwitter:
                 asyncio.gather(*tasks)
             await asyncio.sleep(seconds)
 
-    async def _crabada_selling_alert_loop(self, seconds=5):
+    async def _crabada_selling_alert_loop(self, seconds=10):
         while True:
             try:
                 connection = open_database()
