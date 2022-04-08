d863899ebdc8b1fbc4e02be2ed8d5495a1ca9323
Aittahar Samy <saittahar@uliege.be>
2022-04-08 23:50:46 +0200

    add channel for freshie pincers

--- config.py
+++ config.py
@@ -238,12 +238,18 @@ def is_below_floor_price(price):
     if not os.path.isfile("floor_prices.json"):
         return False
     content = open("floor_prices.json").read()
-    while True:
-        try:
-            floor_prices = json.loads(content)
-            return price <= floor_prices[-1][1]
-        except Exception as e:
-            pass
+    try:
+        floor_prices = json.loads(content)
+        return price <= floor_prices[-1][1]
+    except Exception as e:
+        i = len(content)
+        while i >= 0:
+            try:
+                floor_prices = json.loads(content[:i])
+                return price <= floor_prices[-1][1]
+            except Exception as e:
+                i = i - 1
+        return False
 
 def filter_by_number_of_components(infos_nft: dict, nb_min: int = 15):
     from subclasses import calc_subclass_info
